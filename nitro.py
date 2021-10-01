44f7ceb4ba41d80c3d904d43a24c796a3e566421
MagM1go <68418026+MagM1go@users.noreply.github.com>
2021-10-01 19:55:41 +0800

    Update nitro.py

--- nitro.py
+++ nitro.py
@@ -1,26 +1,26 @@
-import aiohttp
-import asyncio
-import string
-import random
+import aiohttp # pip install aiohttp
+import asyncio # pip install asyncio
+import string # Встроенная библиотека
+import random # Встроенная библиотека
 
 
-a = int(input('How many codes you delivering?: '))
+a = int(input('How many codes you delivering?: ')) # Думаю не стоит говорить, что это
 
-invalid_links = []
-valid_links = []
+invalid_links = [] 
+valid_links = [] # Этих двух массивой то же самое касается
 
-async def main():
-    for i in range(a):
-        j = "".join([i for i in string.ascii_letters + string.digits])
-        codes = "".join(random.choices(j, k=16))
-        async with aiohttp.ClientSession() as session:
-            async with session.get(f'https://discordapp.com/api/v9/entitlements/gift-codes/{codes}?with_application=false&with_subscription_plan=true') as response:
+async def main(): # Создание асинхронной функции
+    for i in range(a): # Цикл
+        j = "".join([i for i in string.ascii_letters + string.digits]) # Проходимся по буковкам и циферкам
+        codes = "".join(random.choices(j, k=16)) # Рандомим их
+        async with aiohttp.ClientSession() as session: # Создание сессии
+            async with session.get(f'https://discordapp.com/api/v9/entitlements/gift-codes/{codes}?with_application=false&with_subscription_plan=true') as response: # Получение запроса уже
                 try:
-                    print(f"Invalid (https://discord.gift/{codes})")
+                    print(f"Invalid (https://discord.gift/{codes})") 
                     invalid_links.append(codes)
                 except:
                     print(f"Valid: https://discord.gift/{codes}")
-                    valid_links.append(f'https://discord.gift/{codes}')
+                    valid_links.append(f'https://discord.gift/{codes}') # 6 Строк выше обычная проверка и занесение в массив
     print(f"""
 
 Valid links: {len(valid_links)}
@@ -28,9 +28,13 @@ Invalid links: {len(invalid_links)}
 
 All valid links: {"".join(valid_links) or "Nothing find."}
 
-""")
+""") # <- Результат всего этого
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+loop.run_until_complete(main()) # Эти две строки для запуска 
 
-input('Press any key for exit')
+input('Press any key for exit') # На случай, если вы будете компилировать в .exe как я
+
+# Ссылка на исходник и .exe файл в описании
+
+# P.S. Это нерабочее, поскольку тут обычный рандом, настоящие генераторы караются баном аккаунт дискорд! Данный "Генератор" обычный розыгрыш, можете давать друзьям чисто по фану, если они у вас, конечно, не прошаренные в этой теме.
